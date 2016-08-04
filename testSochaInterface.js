edd8f596f806c845f9d27064617cbc6a89a3ff02
Valentin <apexys@gmx.de>
2016-08-04 17:51:15 +0200

    Working version of stepping through a live game

--- testSochaInterface.js
+++ testSochaInterface.js
@@ -11,17 +11,59 @@ var V8Interface = java.newInstanceSync("v8interface.V8Interface");
 console.log("Created interface:");
 console.log(V8Interface);
 
-console.log("Testing synchronous interface");
-console.log(V8Interface.testSync());
+var port = 7000;
+V8Interface.setPortSync(port);
 
-console.log("Testing asynchronous interface");
-var t1 = new Date().getTime();
-V8Interface.testAsync((e, r) => {
-	console.log(r)
-	console.log(`Async method call took ${(new Date().getTime()) - t1}ms`);
-});
+var server = V8Interface.getServerSync();
+
+var clname1 = "Name1";
+var clname2 = "Name2";
+
+V8Interface.addClient(clname1, false, true);
+V8Interface.addClient(clname2, false, true);
+
+var gameroom = V8Interface.createGameSync("swc_2016_twixt");
+
+gameroom.pauseSync(true);
+
+gameroom.step(false);
+
+var reservations = V8Interface.getReservationsSync(gameroom);
+
+console.log(reservations);
+
+const spawn = require('child_process').spawn;
+
+var args1 = ["-jar", "twixt_player.jar" ,"--host", "localhost", "--port" , port, "--reservation", reservations[0]];
+var args2 = ["-jar", "twixt_player.jar" ,"--host", "localhost", "--port" , port, "--reservation", reservations[1]];
+
+
+var cmd2 = `java -jar twixt_player.jar --host localhost --port ${port} --reservation ${reservations[1]}`;
+
+var proc1 = spawn('java', args1);
+var proc2 = spawn('java', args2);
+
+proc1.stdout.on('data', data => console.log(""+ data));
+proc2.stdout.on('data', data => console.log(""+ data));
+
+proc1.stderr.on('data', data => console.log("Error:" + data));
+proc2.stderr.on('data', data => console.log("Error:" + data));
+
+//Start client with java -jar -Xmx1250m C:\Users\FrikJonda\Documents\GitHub\socha\deploy\simple_client\twixt_player\jar\java8\twixt_player.jar --host localhost --port 13050 --reservation 1e7bcc3d-fbf7-4a0e-870a-2e8b999d7378
+
+//executable --host localhost --port [port] --reservation [reservation]
 
 //Create game
 //var Game =V8Interface.createGameSync();
 
 //console.log("Created game:");
+
+
+var readLog = () => {
+	V8Interface.getLogEntry((e,l) => {
+		console.log("JAVA LOG: " + l);
+		//readLog();
+	});
+}
+
+readLog();
