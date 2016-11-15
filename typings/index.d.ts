cdbdd3e3139442f8ee48e70f8543b436576d5460
Valentin <apexys@gmx.de>
2016-11-15 17:28:00 +0100

    Removed leftover typings

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/babylon/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
-/// <reference path="modules/requirejs/index.d.ts" />
