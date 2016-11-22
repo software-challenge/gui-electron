533bd10baf0697a1a8fc8d2bcb84763e378837fd
Valentin <apexys@gmx.de>
2016-11-22 15:36:04 +0100

    Fixed promise in Loader

--- code/Loader.ts
+++ code/Loader.ts
@@ -3,10 +3,11 @@ import {Helpers} from "./Helpers";
 
 export namespace Loader{
     export let getReplay = async function(url: string):Promise<Replay>{
+        console.log("loading replay for url " + url);
         let xmldoc = await Helpers.getXmlDocument(url);
         return new Promise<Replay>(resolve => {
             let replayName = /\/?(\w+)\./.exec(url)[1]; //Extract replay name from url
-            return new Replay(replayName, xmldoc);
+            resolve(new Replay(replayName, xmldoc));
         });
     }
 }
