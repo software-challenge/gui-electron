5d31aef239a9bbb5e690b51bd747afbc42458c20
Valentin Buck <apexys@gmx.de>
2017-04-22 19:53:48 +0200

    Adjusted camera a bit

--- code/Viewer.ts
+++ code/Viewer.ts
@@ -221,7 +221,7 @@ export class Viewer{
         this.fieldtypematerialfactory = new FieldTypeMaterialFactory(this.scene);
         this.cameraFocus = BABYLON.Mesh.CreateSphere("dockMesh1",15,0.1,this.scene,false,BABYLON.Mesh.DEFAULTSIDE);
         this.cameraFocus.material = this.fieldtypematerialfactory.getAlphaMaterial();
-        this.camera = new BABYLON.ArcFollowCamera('camera',- 2*Math.PI,1,60,this.cameraFocus,this.scene);
+        this.camera = new BABYLON.ArcFollowCamera('camera',- 2*Math.PI,1,35,this.cameraFocus,this.scene);
         window.camera = this.camera;
         //this.camera = new BABYLON.ArcRotateCamera('camera1',Math.PI, Math.PI, 10,new BABYLON.Vector3(0,0,0),this.scene);
         //this.camera.attachControl(this.canvas, false);
