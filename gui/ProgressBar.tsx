8e2d730e9522e650db014264bef139c2af5b74e8
Sven Koschnicke <s.koschnicke@gfxpro.com>
2017-09-20 10:27:44 +0200

    simplified progress bar

--- gui/ProgressBar.tsx
+++ gui/ProgressBar.tsx
@@ -14,43 +14,15 @@ export class ProgressBar extends React.Component<Props, any> {
     super();
   }
 
-  click(e: any) {
-    //1. Find progress-bar node in case event landed on child node
-    var targetbar = e.target;
-    while (!targetbar.classList.contains('progress-bar')) {
-      targetbar = targetbar.parentNode;
-    }
-
-    //2. Find top left corner of progressbar-loaded-node for accurate reference of top left corner
-    var loadedNode = null;
-    for (var i = 0; i < targetbar.children.length; i++) {
-      if (targetbar.children[i].classList.contains('progress-loaded')) {
-        loadedNode = targetbar.children[i];
-        break;
-      }
-    }
-
-    //Calculate target position of click
-    var targetpos = e.clientX - loadedNode.getBoundingClientRect().left;
-    var targetWidth = targetbar.clientWidth;
-
-    //Calculate turn
-    var clickedTurn = Math.round((targetpos / targetWidth) * (this.props.turnCount - 1));
-    if (clickedTurn > this.props.turnCount) {
-      clickedTurn = this.props.turnCount;
-    }
-    if (this.props.turnCallback) {
-      this.props.turnCallback(clickedTurn);
-    }
+  handleTurnChange(e: any) {
+    this.props.turnCallback(Number(e.target.value));
   }
 
   render() {
-    let currentStyle = { left: this.props.currentTurn * (1.0 / 60.0) * 100.0 + '%' };
     let loadedStyle = { width: (this.props.turnCount - 1) / 60.0 * 100.0 + '%' };
     return (
-      <div className="progress-bar" onClick={this.click.bind(this)}>
-        <div className="progress-loaded" style={loadedStyle} />
-        <div className="progress-position" style={currentStyle} />
+      <div className="progress-bar">
+        <input title="aktueller Zug" className="progress-loaded" type="range" min="0" max={this.props.turnCount - 1} step="1" onChange={(e) => this.handleTurnChange(e)} value={this.props.currentTurn} style={loadedStyle} />
         <div className="round-text">Zug {this.props.currentTurn + 1} / {this.props.turnCount} </div>
       </div>
     );
