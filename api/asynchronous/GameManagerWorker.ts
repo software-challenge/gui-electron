46a9a84c25f3b87cd7214ea018cf4265f0b367f4
Valentin Buck <apexys@gmx.de>
2017-11-26 17:00:51 +0100

    Disable typechecking for messages

--- api/asynchronous/GameManagerWorker.ts
+++ api/asynchronous/GameManagerWorker.ts
@@ -3,4 +3,4 @@ import { AsyncApi } from './AsyncApi';
 import { Logger } from '../Logger';
 
 Logger.getLogger().log("GameManagerWorker", "main", "GameManagerWorker coming online.")
-process.on('message' as any, m => AsyncApi.getAsyncGameManager().handleMessage(m));
+process.on('message' as any, (m: any) => AsyncApi.getAsyncGameManager().handleMessage(m));
