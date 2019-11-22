e9e7fcecaa1d11490ae9dc1bfeab5806b08ac05a
Agent77326 <agent77326@gmail.com>
2019-11-22 11:56:38 +0100

    feat(kiosk): add stop-game button

--- src/gui/Game.tsx
+++ src/gui/Game.tsx
@@ -272,6 +272,10 @@ export class Game extends React.Component<{ gameId: number, name: string, isRepl
                    value={turnActive}
                    step="1"
                    onChange={e => this.updateTurn(Number(e.target.value))}/>
+            <button className="red-button" onClick={e => {
+              const ipc = require('electron').ipcRenderer
+              ipc.send('kioskGameOver', this.props.gameId)
+            }}>Spiel beenden</button>
           </div>
         </div>
       </div>
